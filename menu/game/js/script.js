83bcd1ef38396a7a387118d89cbf366cd3e4873f
Dmitri Kovalenko <kdima0802@gmail.com>
2022-05-02 13:25:39 +0300

    Spichka Alpha 2.0

---
+++ menu/game/js/script.js
@@ -0,0 +1,38 @@
+let elem = document.getElementById("sp");
+let spcount = document.getElementById("count");
+let count = document.getElementById("count2");
+let lvlcnt = document.getElementById("lvlcnt");
+
+let dsBtn = true;
+let cnt = 0;
+
+let level = 1;
+let levelCnt = 10;
+
+var audio = new Audio();
+audio.src = 'game.mp3';
+audio.loop = true;
+audio.autoplay = true;
+
+elem.addEventListener("click", function() {
+    if (dsBtn == true) {
+        dsBtn = false
+
+        spcount.classList.remove("spac");
+        spcount.offsetWidth = spcount.offsetWidth;
+        spcount.classList.add("spac");
+
+        cnt += levelCnt;
+        count.innerHTML = cnt;
+        
+        if (cnt == 500) {
+            level += 1;
+            levelCnt = 20;
+            lvlcnt.innerHTML = levelCnt;
+        }
+
+        setTimeout(function(){
+            dsBtn = true;
+        }, 100);
+    }
+});
---
+++ menu/script.js
@@ -0,0 +1,4 @@
+var audio = new Audio();
+audio.src = 'menu.mp3';
+audio.loop = true;
+audio.autoplay = true;
